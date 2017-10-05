289c63a3a0a7aaccf755f54705ceebd505e03c5e
BNNJ <32491514+BNNJ@users.noreply.github.com>
2017-10-05 16:03:18 +0200

    Add files via upload

--- colle00/colle0X.c
+++ colle00/colle0X.c
@@ -14,42 +14,44 @@ char	*ft_init(const int n)
 		return "BBACCA";
 }
 
+void	ft_print_line(const char *symb, const int x, const int y, const int j)
+{
+	int i;
+
+	i = 1;
+	while (i <= x)
+	{
+		if (j != 1 && j != y) 
+		{
+			if (i == 1 || i == x)
+				ft_putchar(symb[1]);
+			else 
+				ft_putchar(' ');
+		}
+		else
+		{
+			if ((i == 1 || i == x) && j == 1)
+				ft_putchar((i == 1) ? symb[2] : symb[3]);
+			else if ((i == 1 || i == x) && j == y)
+				ft_putchar((i == 1) ? symb[4] : symb[5]);
+			else
+				ft_putchar(symb[0]);
+		}
+		i++;
+	}		
+}
+
 void	ft_core(int n, const int x, const int y)
 {
-	char *symbols;
+	char *symb;
 	int i;
 	int j;
 
 	j = 1;
-	symbols = ft_init(n);
+	symb = ft_init(n);
 	while (j <= y)
 	{
-		i = 1;
-		while (i <= x)
-		{
-			if (j == 1)
-			{
-				if (i == 1 || i == x)
-					ft_putchar((i == 1) ? symbols[2] : symbols[3]);
-				else
-					ft_putchar(symbols[0]);
-			}
-			else if (j == y)
-			{
-				if (i == 1 || i == x)
-					ft_putchar((i == 1) ? symbols[4] : symbols[5]);
-				else
-					ft_putchar(symbols[0]);
-			}
-			else 
-			{
-				if (i == 1 || i == x)
-					ft_putchar(symbols[1]);
-				else 
-					ft_putchar(' ');
-			}
-			i++;
-		}	
+		ft_print_line(symb, x, y, j);
 		if (j != y)
 			ft_putchar('\n');
 		j++;
