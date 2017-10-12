d73e9534ced54635cd984a466a05b08b6b2977af
BNNJ <32491514+BNNJ@users.noreply.github.com>
2017-10-12 14:47:15 +0200

    Update ft_print_comb2.c

--- day02/ex05/ft_print_comb2.c
+++ day02/ex05/ft_print_comb2.c
@@ -5,20 +5,6 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void ft_print_stuff(int i, int j)
-{
-	ft_putchar(i / 10 + '0');
-	ft_putchar(i % 10 + '0');
-	ft_putchar(' ');
-	ft_putchar(j / 10 + '0');
-	ft_putchar(j % 10 + '0');
-	if (i < 98)
-	{
-		ft_putchar(',');
-		ft_putchar(' ');
-	}
-}
-
 void ft_print_comb2()
 {
 	int i = 0;
@@ -29,7 +15,13 @@ void ft_print_comb2()
 		j = i + 1;
 		while(j <= 99)
 		{
-			ft_print_stuff(i, j);
+			ft_putchar(i / 10 + '0');
+			ft_putchar(i % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(j / 10 + '0');
+			ft_putchar(j % 10 + '0');
+			if (i < 98)
+				write(1, ", ", 2);
 			++j;
 		}
 		++i;
