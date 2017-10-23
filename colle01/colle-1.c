3734761b589ca135e29e6dc9fca31294e4fbcb46
BNNJ <32491514+BNNJ@users.noreply.github.com>
2017-10-23 15:01:51 +0200

    Add files via upload

---
+++ colle01/colle-1.c
@@ -0,0 +1,67 @@
+#include "sudoku.h"
+#include <stdio.h>
+
+int		sudoku(char **grid, int row, int col);
+int		is_valid(const char c, char **grid, const int x, const int y);
+
+void	print_grid(char **grid)
+{
+	int i;
+
+	i = 0;
+	while (i < 9)
+	{
+		while (*grid[i])
+		{
+			write(1, grid[i]++, 1);
+			write(1, " ", 1);
+		}
+		write(1, "\n", 1);
+		++i;
+	}
+}
+
+char	**set_grid(char **argv)
+{
+	int col;
+	int row;
+	char **grid;
+
+	row = 0;
+	if (!(grid = malloc(sizeof(char*) * 9)))
+	{
+		write(1, "Error : invalid malloc", 22);
+		return 0;
+	}
+	while (row < 9)
+	{
+		col = 0;
+		grid[row] = malloc(10);
+		while (col < 9)
+		{
+			grid[row][col] = argv[row + 1][col];
+			++col;
+		}
+		grid[row][col] = '\0';
+		++row;
+	}
+	return grid;
+}
+
+
+int		main(int argc, char **argv)
+{
+	char **grid;
+
+	if (argc != 10)
+		write(1, "Error : invalid grid\n", 21);
+ 	else
+ 	{
+		grid = set_grid(argv);
+		if (sudoku(grid, 0, 0))
+			print_grid(grid);
+		else 
+			write(1, "Error : invalid grid\n", 21);
+	}
+	return 0;
+}
---
+++ colle01/sudoku.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int		is_valid(const char c, char **grid, const int y, const int x)
+{
+	int i;
+
+	i = 0;
+	while (i < 9)
+	{
+		if (grid[y][i] == c || grid[i][x] == c)
+			return 0;
+		++i;
+	}
+	if ((c == grid[(y / 3) * 3 + (y + 1) % 3][(x / 3) * 3 + (x + 1) % 3])
+		|| (c == grid[(y / 3) * 3 + (y + 2) % 3][(x / 3) * 3 + (x + 1) % 3])
+		|| (c == grid[(y / 3) * 3 + (y + 1) % 3][(x / 3) * 3 + (x + 2) % 3])
+		|| (c == grid[(y / 3) * 3 + (y + 2) % 3][(x / 3) * 3 + (x + 2) % 3]))
+		return 0;
+	return 1;
+}
+
+int	sudoku(char **grid, int row, int col) 
+{
+	char c;
+
+	c = '1';
+	printf("%d, %d\n", row, col);
+	if (row == 9)
+	{
+		return 1;
+	}
+	if (grid[row][col] != '.')
+	{
+		if (col < 8)
+			return (sudoku(grid, row, col + 1));
+		else
+			return (sudoku(grid, row + 1, 0));
+	}
+	while (c <= '9')
+	{
+		if (is_valid(c, grid, row, col))
+		{
+			grid[row][col] = c;
+			if (col < 8)
+			{
+				if (sudoku(grid, row, col + 1))
+					return 1;
+			}
+			else
+			{
+				if (sudoku(grid, row + 1, 0))
+					return 1;
+			}
+			printf("backtrack\n");
+			grid[row][col] = '.';
+		}
+		++c;
+	}
+	return 0;
+}
