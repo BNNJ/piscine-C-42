3b9363894886c3b874473514cac43db9ae415e4e
BNNJ <32491514+BNNJ@users.noreply.github.com>
2017-10-24 22:37:47 +0200

    Add files via upload

---
+++ match nmatch/match.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int	match(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return match(s1, s2 + 1);
+	if (!*s2 || !*s1)
+		return (*s2 == *s1) ? 1 : 0;
+	return ((match(s1 + 1, s2 + 1) && *s2 == *s1)
+		|| (match(s1 + 1, s2) && *(s2 - 1) == '*'))
+		? 1 : 0;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		if (match(argv[1], argv[2]))
+			write(1, "match !\n", 8);
+		else
+			write(1, "no match :(\n", 12);
+	} 
+	return 0;
+}
