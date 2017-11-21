995842ad75042a7503731194dd966ead26ab4b6d
BNNJ <32491514+BNNJ@users.noreply.github.com>
2017-11-21 20:52:31 +0100

    Add files via upload

---
+++ day10/ex00/includes/libft.h
@@ -0,0 +1,12 @@
+#ifndef		_LIBFT_H_
+# define	_LIBFT_H_
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_swap(int *a, int *b);
+
+#endif	 /* _LIBFT_H_ */
---
+++ day10/ex00/srcs/ft_swap.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+void	ft_swap(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b; 
+	*b = c;
+}
